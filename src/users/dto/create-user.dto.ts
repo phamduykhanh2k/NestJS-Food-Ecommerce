f5757405f278547dfdd69619646cd8e902382bce
“Duy <pdk202016@gmail.com>
2023-10-07 13:59:21 +0700

    updated

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    email: string;
+    password: string;
+    fullname: string;
+    phone: string;
+    address: string;
+    sex: string;
+    role: string;
+    createAt: Date;
+    updateAt: Date;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,34 @@
-import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { genSaltSync, hashSync } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) { }
+
+  getHashPassword = (password: string) => {
+    const salt = genSaltSync(10);
+    const hash = hashSync(password, salt);
+    return hash;
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    const hashPassword = this.getHashPassword(createUserDto.password);
+
+    let result = await this.userModel.create({
+      email: createUserDto.email,
+      password: hashPassword,
+      fullname: createUserDto.fullname,
+      phone: createUserDto.phone,
+      address: createUserDto.address,
+      sex: createUserDto.sex,
+      role: createUserDto.role
+    });
+    return result;
   }
 
   findAll() {
